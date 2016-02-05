8c1cd445a3ab2a342aec1976e2df4cf3e16997b9
kedarjoyner <kedarjoyner@gmail.com>
2016-02-05 15:51:42 -0600

    add scrollspy

--- js/app.js
+++ js/app.js
@@ -2,4 +2,19 @@ $(document).ready(function(){
   $(".icons").hover(function() {
     $(this).effect("bounce", { times: 1 }, 500);
   });
+  // Add smooth scrolling to links in nav
+  $(".navbar-default a.scroll").on('click', function(e){
+      // Prevent default anchor click behavior
+      e.preventDefault();
+
+      //Store hash - pound in anchor tags
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth scrolling
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+      });
+  });
 });
